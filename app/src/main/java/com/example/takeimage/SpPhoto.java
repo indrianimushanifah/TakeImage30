5bb6de963eebcb923ff17ad38ef6f1782e4b34bb
indrianimushanifah <indrianimushanifah30@gmail.com>
2019-07-28 15:06:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/takeimage/SpPhoto.java
@@ -0,0 +1,29 @@
+package com.example.takeimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class SpPhoto extends AppCompatActivity {
+    ImageAdapter myImageAdapter;
+    ArrayList<String> myList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp_photo);
+
+        String imgPath = this.getIntent().getExtras().getString("Path");
+        ImageView iv = (ImageView) findViewById(R.id.singleimage);
+        BitmapFactory.Options myBitmap = new BitmapFactory.Options();
+        myBitmap.inJustDecodeBounds=false;
+        myBitmap.inSampleSize=4;
+        myBitmap.inPurgeable=true;
+        Bitmap bitmap = BitmapFactory.decodeFile(imgPath, myBitmap);
+        iv.setImageBitmap(bitmap);
+    }
+}
